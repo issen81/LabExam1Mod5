95546d5d8fc3a1cdbd6d1ba4d12da2e103ae825d
Yussen <yussen_sg@hotmail.com>
2018-07-06 21:11:52 -0400

    first commit

---
+++ src/main/java/com/example/SimpleWebApplicationExampleMaven/SimpleWebApplicationExampleMavenApplication.java
@@ -0,0 +1,12 @@
+package com.example.SimpleWebApplicationExampleMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplicationExampleMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebApplicationExampleMavenApplication.class, args);
+    }
+}
