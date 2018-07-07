3234844616a7a59d5f1d6d9547e7f48f22d3aec0
Yussen <yussen_sg@hotmail.com>
2018-07-06 21:22:15 -0400

    commit4cherrypick

--- src/main/java/com/example/SimpleWebApplicationExampleMaven/HelloController.java
+++ src/main/java/com/example/SimpleWebApplicationExampleMaven/HelloController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Branch4 cherrypick!";
     }
 }
